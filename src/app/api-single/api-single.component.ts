dcbe774fa8c3578ccb515de5deb661e6cc43ceb3
Janos Dobronszki <dobronszki@gmail.com>
2021-04-14 12:56:22 +0100

    TABLES

--- src/app/api-single/api-single.component.ts
+++ src/app/api-single/api-single.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { ServiceService } from "../service.service";
-import * as types from "../types";
-import { Location } from "@angular/common";
-import { ActivatedRoute } from "@angular/router";
-import { Subject } from "rxjs";
-import * as _ from "lodash";
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ServiceService } from '../service.service';
+import * as types from '../types';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import * as _ from 'lodash';
 import { ToastrService } from 'ngx-toastr';
 import { ExploreService, Service } from '../explore.service';
+import { ReturnStatement } from '@angular/compiler';
 
 const tabNamesToIndex = {
-  "": 0,
+  '': 0,
   logs: 1,
   stats: 2,
   nodes: 3,
@@ -18,20 +19,20 @@ const tabNamesToIndex = {
 };
 
 const tabIndexesToName = {
-  0: "",
-  1: "logs",
-  2: "stats",
-  3: "nodes",
-  4: "traces",
-  5: "events",
+  0: '',
+  1: 'logs',
+  2: 'stats',
+  3: 'nodes',
+  4: 'traces',
+  5: 'events',
 };
 
 @Component({
-  selector: "app-service",
-  templateUrl: "./api-single.component.html",
+  selector: 'app-service',
+  templateUrl: './api-single.component.html',
   styleUrls: [
-    "./api-single.component.css",
-    "../../../node_modules/nvd3/build/nv.d3.css",
+    './api-single.component.css',
+    '../../../node_modules/nvd3/build/nv.d3.css',
   ],
   encapsulation: ViewEncapsulation.None,
 })
@@ -41,8 +42,9 @@ export class ApiSingleComponent implements OnInit {
   stats: types.DebugSnapshot[] = [];
   traceSpans: types.Span[];
   events: types.Event[];
+  openAPI: any = {};
 
-  selectedVersion = "";
+  selectedVersion = '';
   serviceName: string;
   endpointQuery: string;
   intervalId: any;
@@ -66,12 +68,16 @@ export class ApiSingleComponent implements OnInit {
       if (this.intervalId) {
         clearInterval(this.intervalId);
       }
-      this.serviceName = <string>p["id"];
+      this.serviceName = <string>p['id'];
       this.ex.search(this.serviceName).then((servs) => {
-        this.service = servs.filter((s) => s.service.name == this.serviceName)[0];
+        this.service = servs.filter(
+          (s) => s.service.name == this.serviceName
+        )[0];
+        this.openAPI = JSON.parse(this.service.openAPIJSON);
+        console.log(this.openAPI);
       });
       this.loadVersionData();
-      const tab = <string>p["tab"];
+      const tab = <string>p['tab'];
       if (tab) {
         this.selected = tabNamesToIndex[tab];
       }
@@ -82,7 +88,7 @@ export class ApiSingleComponent implements OnInit {
 
   versionSelected(service: types.Service) {
     if (this.selectedVersion == service.version) {
-      this.selectedVersion = "";
+      this.selectedVersion = '';
       return;
     }
     this.selectedVersion = service.version;
@@ -92,7 +98,7 @@ export class ApiSingleComponent implements OnInit {
   tabChange($event: number) {
     this.selected = $event;
     this.location.replaceState(
-      "/" + this.serviceName + "/" + tabIndexesToName[this.selected]
+      '/' + this.serviceName + '/' + tabIndexesToName[this.selected]
     );
     this.tabValueChange.next(this.selected);
   }
@@ -103,5 +109,28 @@ export class ApiSingleComponent implements OnInit {
     }
   }
 
-  code: string = "{}";
+  code: string = '{}';
+
+  pathToRequestSchema(path: string): any {
+    let paths = path.split('/');
+    let endpointName = paths[paths.length - 1];
+    let serviceName = jsUcfirst(this.serviceName);
+
+    if (
+      this.openAPI &&
+      this.openAPI['components']['schemas'] &&
+      this.openAPI['components']['schemas'][
+       endpointName + 'Request'
+      ]
+    ) {
+      return this.openAPI['components']['schemas'][
+        endpointName + 'Request'
+      ];
+    }
+    return {};
+  }
+}
+
+function jsUcfirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
