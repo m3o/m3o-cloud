5db786dd1ca538279f637a29a62629e5e3a7b276
Dominic Wong <dom@m3o.com>
2021-04-20 14:04:08 +0100

    Skeleton API usage with graph

---
+++ src/app/usage.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import * as types from './types';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { CookieService } from 'ngx-cookie-service';
+
+interface ListUsageResponse {
+  usage: Map<string, types.APIUsage>;
+}
+
+@Injectable()
+export class UsageService {
+
+  constructor(
+    private http: HttpClient,
+    private cookie: CookieService,
+  ) {
+  }
+
+
+  listUsage(userID?: string): Promise<Map<string, types.APIUsage>> {
+    return new Promise<Map<string, types.APIUsage>>((resolve, reject) => {
+      return this.http
+        .post<ListUsageResponse>(environment.apiUrl + '/publicapiusage/Read',
+          {
+            customer_id: userID,
+          },
+          {
+            headers: {
+              'Micro-Namespace': 'micro',
+              authorization: this.token()
+            }
+          })
+        .toPromise()
+        .then((listResponse) => {
+          resolve(listResponse.usage);
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
+
+  token(): string {
+    return 'Bearer ' + this.cookie.get('micro_token');
+  }
+
+}
