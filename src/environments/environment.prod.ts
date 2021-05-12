2b4e2de743a5e2d7eb5f823b23c62237f172a825
Dominic Wong <dom@m3o.com>
2021-05-12 15:18:13 +0100

    use live stripe keys but test keys for m3o.com users

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //backendUrl: "https://web.micro.mu/platform",
   apiUrl: "https://api.m3o.com",
-  stripeKey: "pk_test_wuI8wlKwKBUZ9iHnYlQPa8BH",
+  stripeKey: "pk_live_CPlU8hrgrZBGZIg1NZjdpx6p",
+  testStripeKey: "pk_test_wuI8wlKwKBUZ9iHnYlQPa8BH",
   namespace: "micro",
 };
