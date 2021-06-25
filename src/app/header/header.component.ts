ed85623a9a2e090052a872000f32ab7ff29aabd6
Janos Dobronszki <dobronszki@gmail.com>
2021-06-25 14:42:38 +0100

    Remove old code from header component (#122)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,15 +13,6 @@ export class HeaderComponent implements OnInit {
   constructor(private cs: CookieService, private us: UserService) {}
 
   ngOnInit() {
-    let firstVisit = this.cs.get('first_visit');
-    console.log(firstVisit)
-    if (!firstVisit) {
-      alert("wha")
-      let fvisit = Math.floor(Date.now() / 1000);
-      let id = uuid.v4();
-      this.cs.set('tr_id', id, 365, '/', null, null, null);
-      this.cs.set('first_visit', fvisit + '', 365, '/', null, null, null);
-      this.us.track({ id: id, firstVisit: fvisit });
-    }
+
   }
 }
