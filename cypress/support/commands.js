4bb3741cb125f86d25e59b760c54d7528db8d6ae
Martin Virando <martinvirando@hotmail.co.uk>
2021-08-09 15:06:32 +0100

    Added tailwind to the project

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('byCy', (key) => {
+  return cy.get(`[data-cy="${key}"]`);
+});
