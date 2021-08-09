9b49d56ca03f2b1acd81756abbc086937b7fe53c
Martin Virando <martinvirando@hotmail.co.uk>
2021-08-09 16:24:50 +0100

    Removed the old styles on the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,6 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   email: string = '';
