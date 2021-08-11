abfdaf22dfc45d501b7acf11eb8dc0e1841e7315
Janos Dobronszki <dobronszki@gmail.com>
2021-08-11 12:37:55 +0100

    F

--- src/app/google-login/google-login.component.ts
+++ src/app/google-login/google-login.component.ts
@@ -1,38 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
+import { isPlatformBrowser } from '@angular/common';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-google-login',
   templateUrl: './google-login.component.html',
-  styleUrls: ['./google-login.component.css']
+  styleUrls: ['./google-login.component.css'],
 })
 export class GoogleLoginComponent implements OnInit {
+  isBrowser = false;
 
-  constructor(private route: ActivatedRoute, public us: UserService) { }
+  constructor(
+    private route: ActivatedRoute,
+    public us: UserService,
+    @Inject(PLATFORM_ID) platformId: Object,
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
   code: string;
   state: string;
   errorReason: string;
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((queryParams) => {
-
       if (queryParams.get('code')) {
-        this.code = queryParams.get('code')
+        this.code = queryParams.get('code');
       }
 
       if (queryParams.get('state')) {
-        this.state = queryParams.get('state')
+        this.state = queryParams.get('state');
       }
 
       if (queryParams.get('error_reason')) {
-        this.errorReason = queryParams.get('error_reason')
+        this.errorReason = queryParams.get('error_reason');
       }
 
       if (this.code) {
-        this.us.googleOauthCallback(this.code, this.state, this.errorReason)
+        this.us.googleOauthCallback(this.code, this.state, this.errorReason);
       }
     });
   }
-
 }
